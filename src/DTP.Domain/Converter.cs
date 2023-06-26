31acb7c423bf485c87e5af387c7f96a2071b81ff
Artem <gdtechai@gmail.com>
2023-06-26 16:17:33 +0700

    refactor(Program, Convertor): класс Domain.Program переименован в Convertor
    - блоки try catch перенесены во View

---
+++ src/DTP.Domain/Converter.cs
@@ -0,0 +1,26 @@
+namespace DTP.Domain;
+
+/// <summary>
+/// Класс по конвертации файлов из одного формата в другой.
+/// </summary>
+public static class Converter
+{
+	/// <summary>
+	/// Конвертирует файл из .djvu в .pdf.
+	/// </summary>
+	/// <param name="args">Путь к конвертируемому файлу.</param>
+	/// <exception cref="ArgumentException">Конвертируемый файл в неправильном формате.</exception>
+	public static void ConvertDjvuToPdf(string[] args)
+	{
+#if DEBUG
+		const string executableFile = "template.djvu";
+#else
+            var executableFile = args[0];
+#endif
+		if (Path.GetExtension(executableFile) != ".djvu")
+			throw new ArgumentException("File format must be .djvu");
+
+		var outputFilePath = File.ConvertToPdf(executableFile);
+		File.Open(outputFilePath);
+	}
+}
