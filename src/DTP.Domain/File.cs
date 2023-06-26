ddb5d636e76728cd708636aad73952904e58a37f
Artem <gdtechai@gmail.com>
2023-06-27 00:57:39 +0700

    feature(File): НЕ РАБОТАЕТ. реализована конвертация из .djvu в List<Bitmap>

--- src/DTP.Domain/File.cs
+++ src/DTP.Domain/File.cs
@@ -1,69 +1,76 @@
-namespace DTP.Domain;
+﻿namespace DTP.Domain;
 
 /// <summary>
 /// Класс для работы с конвертируемыми файлами.
 /// </summary>
 public static class File
 {
-    /// <summary>
-    /// Расширение выходного файла.
-    /// </summary>
-    private const string OutputExtension = ".pdf";
+	/// <summary>
+	/// Расширение выходного файла.
+	/// </summary>
+	private const string OutputExtension = ".pdf";
 
 
-    /// <summary>
-    /// Конвертирует файл .djvu в .pdf .
-    /// </summary>
-    /// <param name="executableFile">Путь к входному файлу.</param>
-    /// <returns>Путь к выходному файлу.</returns>
-    public static string ConvertToPdf(string executableFile)
-    {
-        var outputFilePath = GetOutputPath(executableFile);
+	/// <summary>
+	/// Конвертирует файл .djvu в .pdf .
+	/// </summary>
+	/// <param name="executableFile">Путь к входному файлу.</param>
+	/// <returns>Путь к выходному файлу.</returns>
+	public static string ConvertToPdf(string executableFile)
+	{
+		var outputFilePath = GetOutputPath(executableFile);
 
-        using var image = Aspose.Imaging.Image.Load(executableFile);
-        var exportOptions = new PdfOptions
-        {
-            PdfDocumentInfo = new PdfDocumentInfo()
-        };
+		try
+		{
+			var document = new DjvuDocument(executableFile);
+			var bitmapPages = document.Pages.Select(page => page.Image).ToList();
 
-        try
-        {
-            image.Save(outputFilePath, exportOptions);
-        }
-        catch
-        {
-            System.IO.File.Delete(outputFilePath);
-            throw;
-        }
-        return outputFilePath;
-    }
 
-    /// <summary>
-    /// Открывает файл программой по умолчанию.
-    /// </summary>
-    /// <param name="filePath">Путь к файлу.</param>
-    public static void Open(string filePath)
-    {
-        Process.Start(
-            new ProcessStartInfo
-            {
-                FileName = filePath,
-                UseShellExecute = true
-            });
-    }
+			////////////////////////////////////////////////////////////
+			//using var image = Aspose.Imaging.Image.Load(executableFile);
+			//var exportOptions = new PdfOptions
+			//{
+			//	PdfDocumentInfo = new PdfDocumentInfo()
+			//};
+			//
+			//image.Save(outputFilePath, exportOptions);
+			//////////////////////////////////////////
+		}
+		catch
+		{
+			System.IO.File.Delete(outputFilePath);
+			throw;
+		}
 
-    /// <summary>
-    /// Возвращает путь к выходному файлу.
-    /// </summary>
-    /// <param name="executableFile">Путь к входному файлу.</param>
-    /// <returns>Путь к выходному файлу.</returns>
-    private static string GetOutputPath(string executableFile)
-    {
-        var directory = Path.GetDirectoryName(executableFile);
-        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(executableFile);
+		return outputFilePath;
+	}
 
-        var outputFilePath = Path.Combine(directory!, fileNameWithoutExtension + OutputExtension);
+	/// <summary>
+	/// Открывает файл программой по умолчанию.
+	/// </summary>
+	/// <param name="filePath">Путь к файлу.</param>
+	public static void Open(string filePath)
+	{
+		Process.Start(
+			new ProcessStartInfo
+			{
+				FileName = filePath,
+				UseShellExecute = true
+			});
+	}
 
-        return outputFilePath;
-    }
+	/// <summary>
+	/// Возвращает путь к выходному файлу.
+	/// </summary>
+	/// <param name="executableFile">Путь к входному файлу.</param>
+	/// <returns>Путь к выходному файлу.</returns>
+	private static string GetOutputPath(string executableFile)
+	{
+		var directory = Path.GetDirectoryName(executableFile);
+		var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(executableFile);
+
+		var outputFilePath = Path.Combine(directory!, fileNameWithoutExtension + OutputExtension);
+
+		return outputFilePath;
+	}
 }
--- src/DTP.Domain/GlobalUsings.cs
+++ src/DTP.Domain/GlobalUsings.cs
@@ -1,3 +1,4 @@
-﻿global using System.Diagnostics;
+﻿global using DjvuNet;
+global using System.Diagnostics;
 global using Aspose.Imaging.ImageOptions;
 global using Aspose.Imaging.FileFormats.Pdf;
