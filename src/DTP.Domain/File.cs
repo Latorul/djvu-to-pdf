ea104927255c13fc2e900e94200d4a5152bf8d02
Artem <gdtechai@gmail.com>
2023-06-28 01:57:44 +0700

    refactor(File): изменён цикл создания страниц на LINQ

--- src/DTP.Domain/File.cs
+++ src/DTP.Domain/File.cs
@@ -11,96 +11,94 @@ namespace DTP.Domain;
 /// </summary>
 public static class File
 {
-	/// <summary>
-	/// Расширение выходного файла.
-	/// </summary>
-	private const string OutputExtension = ".pdf";
+    /// <summary>
+    /// Расширение выходного файла.
+    /// </summary>
+    private const string OutputExtension = ".pdf";
 
 
-	/// <summary>
-	/// Конвертирует файл .djvu в .pdf .
-	/// </summary>
-	/// <param name="executableFile">Путь к входному файлу.</param>
-	/// <returns>Путь к выходному файлу.</returns>
-	public static string ConvertToPdf(string executableFile)
-	{
-		var outputFilePath = GetOutputPath(executableFile);
+    /// <summary>
+    /// Конвертирует файл .djvu в .pdf .
+    /// </summary>
+    /// <param name="executableFile">Путь к входному файлу.</param>
+    /// <returns>Путь к выходному файлу.</returns>
+    public static string ConvertToPdf(string executableFile)
+    {
+        var outputFilePath = GetOutputPath(executableFile);
 
-		try
-		{
-			CreatePdf(executableFile, outputFilePath);
-		}
-		catch
-		{
-			System.IO.File.Delete(outputFilePath);
-			throw;
-		}
+        try
+        {
+            CreatePdf(executableFile, outputFilePath);
+        }
+        catch
+        {
+            System.IO.File.Delete(outputFilePath);
+            throw;
+        }
 
-		return outputFilePath;
-	}
+        return outputFilePath;
+    }
 
-	/// <summary>
-	/// Собирает PDF файл из страниц DJVU файла.
-	/// </summary>
-	/// <param name="inputFilePath">Путь к входному файлу.</param>
-	/// <param name="outputFilePath">Путь к выходному файлу.</param>
-	[SupportedOSPlatform("windows")]
-	private static void CreatePdf(string inputFilePath, string outputFilePath)
-	{
-		var djvuDocument = new DjvuDocument(inputFilePath);
-		var djvuPages = new ConcurrentBag<DjvuPage>(djvuDocument.Pages).Reverse();
+    /// <summary>
+    /// Собирает PDF файл из страниц DJVU файла.
+    /// </summary>
+    /// <param name="inputFilePath">Путь к входному файлу.</param>
+    /// <param name="outputFilePath">Путь к выходному файлу.</param>
+    [SupportedOSPlatform("windows")]
+    private static void CreatePdf(string inputFilePath, string outputFilePath)
+    {
+        var djvuDocument = new DjvuDocument(inputFilePath);
+        var unsafeDjvuPages = djvuDocument.Pages.ToList();
 
-		using var pdfDocument = new PdfDocument();
-		foreach (var page in djvuPages)
-		{
-			pdfDocument.AddPage();
-		}
+        using var pdfDocument = new PdfDocument();
+        unsafeDjvuPages.ForEach(_ => { pdfDocument.AddPage(); });
 
-		Parallel.For(0, djvuPages.Count(), (int i) =>
-		{
-			var djvuImage = djvuPages.ElementAt(i).Image;
+        var djvuPages = new ConcurrentBag<DjvuPage>(djvuDocument.Pages).Reverse();
+        Parallel.For(0, djvuPages.Count(), i =>
+        {
+            var djvuImage = djvuPages.ElementAt(i).Image;
 
-			using var stream = new MemoryStream();
-			djvuImage.Save(stream, ImageFormat.Bmp);
-			using var xImage = XImage.FromStream(stream);
+            using var stream = new MemoryStream();
+            djvuImage.Save(stream, ImageFormat.Bmp);
+            using var xImage = XImage.FromStream(stream);
 
-			var page = pdfDocument.Pages[i];
-			page.Width = djvuImage.Width;
-			page.Height = djvuImage.Height;
+            var page = pdfDocument.Pages[i];
+            page.Width = djvuImage.Width;
+            page.Height = djvuImage.Height;
 
-			XGraphics graphics = XGraphics.FromPdfPage(page);
-			graphics.DrawImage(xImage, 0, 0, page.Width, page.Height);
-		});
+            XGraphics graphics = XGraphics.FromPdfPage(page);
+            graphics.DrawImage(xImage, 0, 0, page.Width, page.Height);
+        });
 
-		pdfDocument.Save(outputFilePath);
-	}
+        pdfDocument.Save(outputFilePath);
+    }
 
-	/// <summary>
-	/// Открывает файл программой по умолчанию.
-	/// </summary>
-	/// <param name="filePath">Путь к файлу.</param>
-	public static void Open(string filePath)
-	{
-		Process.Start(
-			new ProcessStartInfo
-			{
-				FileName = filePath,
-				UseShellExecute = true
-			});
-	}
+    /// <summary>
+    /// Открывает файл программой по умолчанию.
+    /// </summary>
+    /// <param name="filePath">Путь к файлу.</param>
+    public static void Open(string filePath)
+    {
+        Process.Start(
+            new ProcessStartInfo
+            {
+                FileName = filePath,
+                UseShellExecute = true
+            });
+    }
 
-	/// <summary>
-	/// Возвращает путь к выходному файлу.
-	/// </summary>
-	/// <param name="executableFile">Путь к входному файлу.</param>
-	/// <returns>Путь к выходному файлу.</returns>
-	private static string GetOutputPath(string executableFile)
-	{
-		var directory = Path.GetDirectoryName(executableFile);
-		var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(executableFile);
+    /// <summary>
+    /// Возвращает путь к выходному файлу.
+    /// </summary>
+    /// <param name="executableFile">Путь к входному файлу.</param>
+    /// <returns>Путь к выходному файлу.</returns>
+    private static string GetOutputPath(string executableFile)
+    {
+        var directory = Path.GetDirectoryName(executableFile);
+        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(executableFile);
 
-		var outputFilePath = Path.Combine(directory!, fileNameWithoutExtension + OutputExtension);
+        var outputFilePath = Path.Combine(directory!, fileNameWithoutExtension + OutputExtension);
 
-		return outputFilePath;
-	}
+        return outputFilePath;
+    }
 }
