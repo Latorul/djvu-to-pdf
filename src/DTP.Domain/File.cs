bd5d718c7fadec3ec45ca8018803a63c28d7bf63
Artem <gdtechai@gmail.com>
2023-06-28 01:09:41 +0700

    feature(File): добавлена многопоточность в конвертацию страниц

--- src/DTP.Domain/File.cs
+++ src/DTP.Domain/File.cs
@@ -1,96 +1,106 @@
-﻿namespace DTP.Domain;
+﻿using DjvuNet;
+using DjvuNet.Graphics;
+using PdfSharp.Pdf;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace DTP.Domain;
 
 /// <summary>
 /// Класс для работы с конвертируемыми файлами.
 /// </summary>
 public static class File
 {
-    /// <summary>
-    /// Расширение выходного файла.
-    /// </summary>
-    private const string OutputExtension = ".pdf";
+	/// <summary>
+	/// Расширение выходного файла.
+	/// </summary>
+	private const string OutputExtension = ".pdf";
 
 
-    /// <summary>
-    /// Конвертирует файл .djvu в .pdf .
-    /// </summary>
-    /// <param name="executableFile">Путь к входному файлу.</param>
-    /// <returns>Путь к выходному файлу.</returns>
-    public static string ConvertToPdf(string executableFile)
-    {
-        var outputFilePath = GetOutputPath(executableFile);
+	/// <summary>
+	/// Конвертирует файл .djvu в .pdf .
+	/// </summary>
+	/// <param name="executableFile">Путь к входному файлу.</param>
+	/// <returns>Путь к выходному файлу.</returns>
+	public static string ConvertToPdf(string executableFile)
+	{
+		var outputFilePath = GetOutputPath(executableFile);
 
-        try
-        {
-            CreatePdf(executableFile, outputFilePath);
-        }
-        catch
-        {
-            System.IO.File.Delete(outputFilePath);
-            throw;
-        }
+		try
+		{
+			CreatePdf(executableFile, outputFilePath);
+		}
+		catch
+		{
+			System.IO.File.Delete(outputFilePath);
+			throw;
+		}
 
-        return outputFilePath;
-    }
+		return outputFilePath;
+	}
 
-    /// <summary>
-    /// Собирает PDF файл из страниц DJVU файла.
-    /// </summary>
-    /// <param name="inputFilePath">Путь к входному файлу.</param>
-    /// <param name="outputFilePath">Путь к выходному файлу.</param>
-    [SupportedOSPlatform("windows")]
-    private static void CreatePdf(string inputFilePath, string outputFilePath)
-    {
-        var djvuDocument = new DjvuDocument(inputFilePath);
-        var djvuPages = djvuDocument.Pages;
-        
-        using var pdfDocument = new PdfDocument();
+	/// <summary>
+	/// Собирает PDF файл из страниц DJVU файла.
+	/// </summary>
+	/// <param name="inputFilePath">Путь к входному файлу.</param>
+	/// <param name="outputFilePath">Путь к выходному файлу.</param>
+	[SupportedOSPlatform("windows")]
+	private static void CreatePdf(string inputFilePath, string outputFilePath)
+	{
+		var djvuDocument = new DjvuDocument(inputFilePath);
+		var djvuPages = new ConcurrentBag<DjvuPage>(djvuDocument.Pages).Reverse();
 
-        foreach (DjvuPage? djvuPage in djvuPages)
-        {
-            var djvuImage = djvuPage.Image;
-            
-            using var stream = new MemoryStream();
-            djvuImage.Save(stream, ImageFormat.Bmp);
-            using var xImage = XImage.FromStream(stream);
-            
-            var page = pdfDocument.AddPage();
-            page.Width = djvuImage.Width;
-            page.Height = djvuImage.Height;
+		using var pdfDocument = new PdfDocument();
+		foreach (var page in djvuPages)
+		{
+			pdfDocument.AddPage();
+		}
 
-            XGraphics graphics = XGraphics.FromPdfPage(page);
-            graphics.DrawImage(xImage, 0, 0, page.Width, page.Height);
-        }
+		Parallel.For(0, djvuPages.Count(), (int i) =>
+		{
+			var djvuImage = djvuPages.ElementAt(i).Image;
 
-        pdfDocument.Save(outputFilePath);
-    }
+			using var stream = new MemoryStream();
+			djvuImage.Save(stream, ImageFormat.Bmp);
+			using var xImage = XImage.FromStream(stream);
 
-    /// <summary>
-    /// Открывает файл программой по умолчанию.
-    /// </summary>
-    /// <param name="filePath">Путь к файлу.</param>
-    public static void Open(string filePath)
-    {
-        Process.Start(
-            new ProcessStartInfo
-            {
-                FileName = filePath,
-                UseShellExecute = true
-            });
-    }
+			var page = pdfDocument.Pages[i];
+			page.Width = djvuImage.Width;
+			page.Height = djvuImage.Height;
 
-    /// <summary>
-    /// Возвращает путь к выходному файлу.
-    /// </summary>
-    /// <param name="executableFile">Путь к входному файлу.</param>
-    /// <returns>Путь к выходному файлу.</returns>
-    private static string GetOutputPath(string executableFile)
-    {
-        var directory = Path.GetDirectoryName(executableFile);
-        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(executableFile);
+			XGraphics graphics = XGraphics.FromPdfPage(page);
+			graphics.DrawImage(xImage, 0, 0, page.Width, page.Height);
+		});
 
-        var outputFilePath = Path.Combine(directory!, fileNameWithoutExtension + OutputExtension);
+		pdfDocument.Save(outputFilePath);
+	}
 
-        return outputFilePath;
-    }
+	/// <summary>
+	/// Открывает файл программой по умолчанию.
+	/// </summary>
+	/// <param name="filePath">Путь к файлу.</param>
+	public static void Open(string filePath)
+	{
+		Process.Start(
+			new ProcessStartInfo
+			{
+				FileName = filePath,
+				UseShellExecute = true
+			});
+	}
+
+	/// <summary>
+	/// Возвращает путь к выходному файлу.
+	/// </summary>
+	/// <param name="executableFile">Путь к входному файлу.</param>
+	/// <returns>Путь к выходному файлу.</returns>
+	private static string GetOutputPath(string executableFile)
+	{
+		var directory = Path.GetDirectoryName(executableFile);
+		var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(executableFile);
+
+		var outputFilePath = Path.Combine(directory!, fileNameWithoutExtension + OutputExtension);
+
+		return outputFilePath;
+	}
 }
